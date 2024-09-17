9fc5667ecd8e93a6d45f5e6723b09cfe288747ce
MpilonhleMtungwa <mtungwampilonhle@gmail.com>
2024-09-17 18:47:55 +0200

    header and sidebar

---
+++ src/components/Sidebar.js
@@ -0,0 +1,24 @@
+import ".styles/sidebar.css";
+
+function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <section className="popular-posts">
+        <h2>popular posts</h2>
+        <PostCard title="Post 1" />
+        <PostCard title="Post 2" />
+      </section>
+
+      <section className="create-post">
+        <button>Create Posts</button>
+      </section>
+
+      <section className="search-posts">
+        <h2>Search Posts</h2>
+        <input type="text" placeholder="Search..." />
+      </section>
+    </aside>
+  );
+}
+
+export default Sidebar;
---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+import ".styles/header.css";
+
+function header() {
+  return (
+    <header className="header">
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Posts</li>
+          <li>Login</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default header;
